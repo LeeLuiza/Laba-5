9b8c16d7a39435ce16651a40dba4f1a26e4fcbee
LeeLuiza <116955531+LeeLuiza@users.noreply.github.com>
2022-12-09 21:56:13 +0200

    Добавьте файлы проекта.

---
+++ Лаба 5/Program.cs
@@ -0,0 +1,27 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите номер задания");
+        {
+            Console.WriteLine("Для выхода нажмите - '0'");
+            string point = Console.ReadLine();
+            switch (point)
+            {
+
+                case "1":
+                    L1();
+                    break;
+                case "2":
+                    L2();
+                    break;
+                case "3":
+                    L3();
+                    break;
+                case "4":
+                    L4();
+                    break;
+            }
+        }
+    }
+}
