33aa6bf0e087376ce8738a0fba479b20db05de3d
LeeLuiza <116955531+LeeLuiza@users.noreply.github.com>
2022-12-10 21:53:58 +0200

    task2

--- Лаба 5/Program.cs
+++ Лаба 5/Program.cs
@@ -23,5 +23,30 @@
                     break;
             }
         }
+        static void L2()
+        {
+            string x = Console.ReadLine();
+
+            char[] a = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-' };
+            int[] c = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int summ = 0;
+            for (int t = 0; t < x.Length; t++)
+            {
+                int k = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (x[t] == a[i]) k = k + 1;
+                if (k == 0)
+                {
+                    Console.WriteLine("Число введено не корректно");
+                    break;
+                }
+
+                for (int h = 0; h < c.Length; h++)  //считаем сумму
+                    if (x[t] == a[h]) summ += c[h];
+            }
+
+            Console.Write("Сумма ");
+            Console.Write(summ);
+        }
     }
 }
